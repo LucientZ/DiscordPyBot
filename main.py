4c692f83ab9265825f3274f7aac63481257b3b1b
LucientZ <koblenzpoisiden@gmail.com>
2022-04-10 01:40:04 -0500

    Added token handling in main

--- main.py
+++ main.py
@@ -16,16 +16,30 @@ async def on_ready():
     print('I can talk to friends now! :)')
 
 # Login information for the bot requires a token.
-# Because this repository is public, this token is inputted by the user.
-TOKEN = input('Please enter the bot token: ')
+# token is taken from a file named 'token.dat'
+# If this file does not exist, user will be prompted to input token
+is_running = False
 
-# TODO: Come up with more elegant method of user-input verification
-# This will work for the time being
-while(True):
+# TODO: Figure out how to recover from 'Improper token has been passed.' error
+# Error turns into RuntimeError('Even loop is closed')
+while(not is_running):
     try:
+        TOKEN = dt.get_token()
         client.run(TOKEN)
-        break
+        is_running = True
+    except discord.LoginFailure as e:
+        #This error is raised when the token is not valid
+        print("[ERROR] Issue logging into bot:",e,'\n')
+        print("The program will exit.")
+        choice = input("Would you like to enter a new token? [Y/n] ")
+        while(choice != "Y" and choice != "n"):
+            choice = input("\nWould you like to write a new token? [Y/n] ")    
+        if(choice == "Y"):
+            TOKEN = input("Please enter bot token: ")
+            dt.write_token(TOKEN)
+        print("Terminating program...\n")
+        exit()
     except Exception as e:
         print("[ERROR] Issue logging into bot:",e,'\n')
-        print("Exiting program...")
+        print("Terminating program...\n")
         exit()
