42b822414e2d4582b20c894c7ce628810c915ac2
LucientZ <koblenzpoisiden@gmail.com>
2022-04-10 00:23:50 -0500

    edited datahandling and added unit tests

---
+++ datahandling_unittests.py
@@ -0,0 +1,48 @@
+from datahandling import *
+import os
+
+#Test 1: add a token
+write_token("awesome")
+try:
+    myfile = open('token.dat','r')
+    token = myfile.read()
+    if(token != "awesome"):
+        raise Exception("File contents not string 'awesome'")
+    myfile.close()
+    print("[Test 1] passed")
+except Exception as e:
+    print("[Test 1] passed\n","[ERROR]:",e)
+
+
+#Test 2: add a new token to replace the old token
+write_token("new_token")
+try:
+    myfile = open('token.dat','r')
+    token = myfile.read()
+    if(token != "new_token"):
+        raise Exception("File contents not string 'awesome'")
+    myfile.close()
+    print("[Test 2] passed")
+except Exception as e:
+    print("[Test 2] passed\n","[ERROR]:",e)
+
+
+write_token("new_token")
+TOKEN = get_token()
+if(TOKEN == "new_token"):
+    print("[Test 3] passed")
+else:
+    print("[Test 3] failed")
+    print("TOKEN =", TOKEN)
+os.remove("token.dat")
+
+print("\nPlease type 'Y' on this next section and enter 'new_token' when prompted for a token")
+TOKEN = get_token()
+
+if(TOKEN == "new_token"):
+    print("[Test 4] passed")
+else:
+    print("[Test 4] failed")
+    print("TOKEN =", TOKEN)
+
+
