57f0173dc8444ff2941900ab5842b277b5499c11
LucientZ <koblenzpoisiden@gmail.com>
2022-04-26 11:57:01 -0500

    added fitnessgram copypasta when someone says 'run' or 'fitness'

--- main.py
+++ main.py
@@ -19,6 +19,9 @@ async def on_message(message):
     if message.author == client.user:
         return
 
+    if 'run' in message.content.lower() or 'fitness' in message.content.lower():
+        await message.channel.send("The FitnessGram Pacer Test is a multistage aerobic capacity test that progressively gets more difficult as it continues. The 20 meter pacer test will begin in 30 seconds. Line up at the start. The running speed starts slowly but gets faster each minute after you hear this signal bodeboop. A sing lap should be completed every time you hear this sound. ding Remember to run in a straight line and run as long as possible. The second time you fail to complete a lap before the sound, your test is over. The test will begin on the word start. On your mark. Get ready!… Start.")
+
     if 'morbius' in message.content.lower():
         await message.channel.send(morbius())
     
