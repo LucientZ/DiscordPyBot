879ee7886cdb87f461281c48a31cdc35fe3e71e8
LucientZ <koblenzpoisiden@gmail.com>
2022-04-26 11:26:25 -0500

    Modified helper and textfunctions comments

--- helper.py
+++ helper.py
@@ -1,6 +1,7 @@
+# Used for console formatting
 class colors:
     BLUE = "\033[94m"
     GREEN = "\033[92m"
     RED = "\033[91m"
     BOLD = "\033[1m"
-    END = "\033[0m" #sets color back to white
+    END = "\033[0m" # Resets output to default
