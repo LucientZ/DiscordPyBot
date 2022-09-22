99bcad8dbff8fca664b0293d73bf355db8b364fe
LucientZ <koblenzpoisiden@gmail.com>
2022-09-22 01:33:27 -0500

    Added support for global syncing of slash commands and asks user if syncing should occur

--- main.py
+++ main.py
@@ -2,7 +2,8 @@
 import discord
 from discord import app_commands
 from discord.ext import commands
-
+import time
+from datetime import datetime
 
 # Other Files
 import datahandling as dt
@@ -13,32 +14,43 @@ from helper import *
 class aclient(discord.Client):
     def __init__(self):
         super().__init__(command_prefix = 's-', activity = discord.Game(name = "s-help"), intents=discord.Intents.all())
-        self.synced = False
+        choice = ""
+        while choice.lower() != "y" and choice.lower() != "n":
+            choice = input("Would you like to sync the bot? (Only do this if the bot has been updated or a command has changed) [Y/n] ")
+
+        if choice.lower() == "y":
+            self.synced = False
+        else:
+            self.synced = True
 
     async def on_ready(self):
         await self.wait_until_ready()
         if not self.synced:
-            await tree.sync(guild = discord.Object(int(dt.get_token(".sync", "Syncing Commands"))))
+            print(f"{cl.GREY}{cl.BOLD}{datetime.now()}{cl.BLUE} INFO{cl.END}    Awaiting command tree syncing...")
+            a = time.time()
+            await tree.sync()
+            b = time.time()
+            print(f"{cl.GREY}{cl.BOLD}{datetime.now()}{cl.BLUE} INFO{cl.END}    Tree Synced. Elapsed time: {int((b - a) * 100) / 100} seconds")
             self.synced = True
-        print(f"I exist as {self.user} and can talk to people! :D")
+        print(f"I exist as '{self.user}' and can talk to people! :D")
 
     async def on_connect(self):
-        print("I exist :)")
+        print("I'm initializing myself :)")
 
 
 client = aclient()
 tree = app_commands.CommandTree(client)
 
-@tree.command(name = "pings", description = "Responds and states client latency in milliseconds", guild = discord.Object(int(dt.get_token(".sync", "Syncing Commands"))))
+@tree.command(name = "ping", description = "Responds and states client latency in milliseconds")
 async def ping(ctx: discord.Interaction):
-    await ctx.response.send_message(f"Pong! {int(client.latency * 1000)} ms")
+    await ctx.response.send_message(f"Pong!\nClient Latency: {int(client.latency * 1000)} ms")
 
 
 def main():
     try:
         dt.init_guild_config()
         dt.init_file("textdata/copypasta.dat")
-        TOKEN = dt.get_token(".token")
+        TOKEN = dt.get_token(".token", "Logging In")
         client.run(TOKEN)
     except discord.LoginFailure as e:
         #This error is raised when the token is not valid
