5c411a4fb4b066245dd35f89cbe5802d8450278c
LucientZ <koblenzpoisiden@gmail.com>
2022-09-22 13:03:37 -0500

    Added on_message() interactions which were available in the previous version of the bot

--- main.py
+++ main.py
@@ -42,6 +42,33 @@ class aclient(discord.Client):
     async def on_connect(self):
         print("I'm initializing myself :)")
 
+    async def on_message(self, ctx: discord.Interaction):
+        # Bot doesn't respond to itself
+        # Protection for if a message parsed by the bot is too long.
+        ctx_size = len(ctx.content)
+        if ctx.author.bot or ctx_size > 1500:
+            return
+
+        # Current response in DMs is simply to say "Hello"
+        if isinstance(ctx.channel, discord.channel.DMChannel):
+            await ctx.channel.send("Hello")
+        # Checks if the first two characters of the message starts with 's-' to either parse message as command or regular message
+        elif(not ctx.content.lower()[0:2] == "s-"):
+            if 'sus' in ctx.content.lower() and not dt.is_blacklisted("sus", str(ctx.guild.id), str(ctx.channel.id)):
+                if(not ctx_size > 665):
+                    await ctx.channel.send("Amogus detected: " + format_msg(ctx.content, 'sus','**'))
+                else:
+                    await ctx.channel.send("__Amogus Detected in Message__")
+                    await ctx.channel.send(ctx.content)
+            elif 'morb' in ctx.content.lower() and not dt.is_blacklisted("morbius", str(ctx.guild.id), str(ctx.channel.id)):
+                await ctx.channel.send(morbius())
+            elif 'sad' in ctx.content.lower() and not dt.is_blacklisted("sad", str(ctx.guild.id), str(ctx.channel.id)):
+                await ctx.channel.send("https://cdn.discordapp.com/attachments/390692666897203211/970382349617483856/293.jpg")
+            elif 'trade' in ctx.content.lower() and not dt.is_blacklisted("trade", str(ctx.guild.id), str(ctx.channel.id)):
+                await ctx.channel.send("yeah i trade :smile:")
+            elif (('do' in ctx.content.lower()[ctx_size - 2:] or 'doing' in ctx.content.lower()[ctx_size - 5:] or 'doin' in ctx.content.lower()[ctx_size - 4:] or 'did' in ctx.content.lower()[ctx_size - 3:] or 'wyd' in ctx.content.lower()[ctx_size - 3:]) or ('do?' in ctx.content.lower()[ctx_size - 3:] or 'doing?' in ctx.content.lower()[ctx_size - 6:] or 'doin?' in ctx.content.lower()[ctx_size - 5:] or 'did?' in ctx.content.lower()[ctx_size - 4:] or 'wyd?' in ctx.content.lower()[ctx_size - 4:])) and not dt.is_blacklisted("mom", str(ctx.guild.id), str(ctx.channel.id)):
+                await ctx.channel.send(mom())
+
 
 client = aclient()
 tree = app_commands.CommandTree(client)
